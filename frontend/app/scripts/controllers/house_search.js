9eaf867ae870332ef85200b0bddc1d2fd6f2b891
mkreiser <mkreise2@illinois.edu>
2015-10-20 21:18:59 -0500

    House deleted

--- frontend/app/scripts/controllers/house_search.js
+++ frontend/app/scripts/controllers/house_search.js
@@ -3,8 +3,17 @@
 angular.module('frontendApp')
 
 .controller('houseSearchCtrl', function($rootScope, $scope) {
-	$scope.editHouse = function(house) {
-		$rootScope.editHouseId = house;
+	$scope.editHouse = function(houseId) {
+		$rootScope.editHouseId = houseId;
 		$rootScope.goToState('edit_house');
 	};
+
+	$scope.deleteHouse = function(houseId) {
+		$http.delete(
+			'http://localhost:8000/houses/deleteHouse/' + houseId + '/'
+		).success(function() {
+			$rootScope.showSimpleToast('House deleted!');
+			$rootScope.goToState('houses');
+		});
+	}
 });
